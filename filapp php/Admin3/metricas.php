ddb6512e2b1b4c176142550d60839f942f91a387
Guido Capucciati <guidoca_94@hotmail.com>
2024-12-09 22:17:08 -0300

    modificaciones php con gaston

--- filapp php/Admin3/metricas.php
+++ filapp php/Admin3/metricas.php
@@ -3,13 +3,24 @@ require_once 'Login/session_habilitada.php';
 include 'headersup.php';
 require_once '../php/Conexiones/conn.php';
 
+
+
 /*
 	Selecciono los clientes en espera por motivo,
 	el problema es como saber cuantos motivos hay???!
 	tendria que primero hacer un array con los motivos
 */
+
+
+$sqlmotivos = "SELECT motivo, count(*) as cuenta FROM clientes GROUP by motivo";
+$resultado = mysqli_query($link, $sqlmotivos) or die(mysqli_error($link));
+
 // Crear un array para guardar los resultados
 $resultados = [];
+
+/* while($fila = mysqli_fetch_array($resultado)){
+	$resultados[] = $fila[0];
+} */
 
 // Recorremos cada motivo y realizamos la consulta correspondiente
 foreach ($motivos as $motivo) {
@@ -22,10 +33,6 @@ foreach ($motivos as $motivo) {
     // Guardamos el resultado en el array
     $resultados[$motivoNombre] = $clientesPorMotivo;
 }
-
-// Ahora el array $resultados tiene la cantidad de clientes por cada motivo
-
-
 
 
 ?>
@@ -50,10 +57,20 @@ foreach ($motivos as $motivo) {
 				<canvas id="empleados" width="100px" height="100px"></canvas>
 			</div>
 		</section>
+
+		
+		<section class="full-width header-well" style="display: flex;">
+			<div style="width: 30%; height: 30%;">
+				<canvas id="Motivos" width="100px" height="100px"></canvas>
+			</div>
+			<div style="width: 30%; height: 30%;">
+				<canvas id="empleados" width="100px" height="100px"></canvas>
+			</div>
+		</section>
 		<section class="full-width text-center" style="padding: 40px 0;">
     <h3 class="text-center tittles">Bienvenido</h3>
     
-    <!-- Aquí se generan los articles dinámicamente -->
+    <!-- Aquí se generan los articles dinámicamente NO FUNCIONA -->
     <?php foreach ($resultados as $motivo => $cantidad): ?>
         <article class="full-width tile">
             <div class="tile-text">
@@ -65,6 +82,19 @@ foreach ($motivos as $motivo) {
             <i class="zmdi zmdi-account tile-icon"></i>
         </article>
     <?php endforeach; ?>
+
+	    <!-- Aquí se generan los articles dinámicamente -->
+		<?php while($fila = mysqli_fetch_array($resultado)){ ?>
+        <article class="full-width tile">
+            <div class="tile-text">
+                <span class="text-condensedLight">
+                    <?php echo $fila[1]; ?><br>
+                    <small>Clientes atendidos por <?php echo $fila[0]; ?></small>
+                </span>
+            </div>
+            <i class="zmdi zmdi-account tile-icon"></i>
+        </article>
+    <?php } ?>
     
 </section>
 <?php 
--- filapp php/Admin3/metricasEmpleado.php
+++ filapp php/Admin3/metricasEmpleado.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'Login/session_habilitada.php';
 include 'headerEmp.php';
-
 require_once '../php/Conexiones/conn.php';
 /*
  ?>
@@ -19,23 +18,27 @@ require_once '../php/Conexiones/conn.php';
  	</section>
 	*/
 
+	$user = $_SESSION['user'];
+
+	date_default_timezone_set('America/Argentina/Buenos_Aires');
+	$hoy = date("d/m/y");
 	
-	
-	/*Cuento los clientes que ya esten atendidos y que el usuarioDeAtencion sea igual al usuario en sesion*/
-	$sql2 = "SELECT count(enEspera) from clientes where enEspera > 2 AND usuarioAtencion = '" . $_SESSION['user'] . "'";
-	$respuesta2 = mysqli_query($link, $sql2) or die(mysqli_error($link));
-	$clientes2 = mysqli_fetch_array($respuesta2);
 	
 	/*Busco el tiempo promedio de atencion del usuario en sesion */
-	$sqlTma = "SELECT SEC_TO_TIME(FLOOR(AVG(TIMESTAMPDIFF(SECOND, inicioAtencion, finAtencion)))) AS tiempo_medio FROM clientes AND usuarioAtencion = '" . $_SESSION['user'] . "'";
+	$sqlTma = "SELECT SEC_TO_TIME(FLOOR(AVG(TIMESTAMPDIFF(SECOND, inicioAtencion, finAtencion)))) AS tiempo_medio FROM clientes WHERE usuarioDeAtencion = '".$user."'";
 	$respuestaTMA = mysqli_query($link, $sqlTma) or die(mysqli_error($link));
 	$tma = mysqli_fetch_array($respuestaTMA);
 	
+
+	/*Cuento los clientes que ya esten atendidos y que el usuarioDeAtencion sea igual al usuario en sesion*/
+		$sql2 = "SELECT count(enEspera) as contar from clientes where enEspera > 2 AND usuarioDeAtencion = '".$user."'";
+		$respuesta2 = mysqli_query($link, $sql2) or die(mysqli_error($link));
+		$clientes2 = mysqli_fetch_array($respuesta2);  
 	
 	?>
 	
 			<section class="full-width text-center" style="padding: 40px 0;">
-				<h3 class="text-center tittles">Bienvenido</h3>
+				<h3 class="text-center tittles">Metricas del dia de hoy  <?php echo $hoy?></h3>
 				<!-- Tiles -->
 				
 				<article class="full-width tile">
