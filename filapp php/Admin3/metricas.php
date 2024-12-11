23f6ee3d2d750f475e0298efa4d3dbcb8af91dcb
Thuomas <Gastonmansilla9@gmail.com>
2024-12-11 16:12:07 -0300

    Re-organizacion pantalla metricas supervisor

--- filapp php/Admin3/metricas.php
+++ filapp php/Admin3/metricas.php
@@ -15,63 +15,79 @@ require_once '../php/Conexiones/conn.php';
 $sqlmotivos = "SELECT motivo, count(*) as cuenta FROM clientes GROUP by motivo";
 $resultado = mysqli_query($link, $sqlmotivos) or die(mysqli_error($link));
 
+$sql2 = "SELECT count(enEspera) from clientes where enEspera > 2";
+$respuesta2 = mysqli_query($link, $sql2) or die(mysqli_error($link));
+$clientes2 = mysqli_fetch_array($respuesta2);
+/* 
 // Crear un array para guardar los resultados
 $resultados = [];
 
-/* while($fila = mysqli_fetch_array($resultado)){
+while($fila = mysqli_fetch_array($resultado)){
 	$resultados[] = $fila[0];
-} */
+} 
 
-// Recorremos cada motivo y realizamos la consulta correspondiente
+
 foreach ($motivos as $motivo) {
-    $motivoNombre = $motivo['motivo']; // Obtener el nombre del motivo
+    $motivoNombre = $motivo['motivo']; 
     $sql = "SELECT COUNT(enEspera) FROM clientes WHERE enEspera < 2 AND motivo LIKE '$motivoNombre'";
     
     $respuesta = mysqli_query($link, $sql) or die(mysqli_error($link));
-    $clientesPorMotivo = mysqli_fetch_array($respuesta)[0]; // Obtener el resultado del conteo
+    $clientesPorMotivo = mysqli_fetch_array($respuesta)[0];
     
-    // Guardamos el resultado en el array
+   
     $resultados[$motivoNombre] = $clientesPorMotivo;
-}
+}*/
 
 
 ?>
 
 
 		<section class="full-width header-well">
-			<div class="full-width header-well-icon">
-				<i class="zmdi zmdi-alert-circle-o"></i>
-			</div>
-			<div class="full-width header-well-text">
-				<p class="text-condensedLight">
-					Sitio en construccion	
-				</p>
-			</div>
+		<article class="full-width tile">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<?php echo $clientes2[0]; ?><br>
+						<small>Clientes atendidos hoy</small>
+					</span>
+				</div>
+				<i class="zmdi zmdi-accounts tile-icon"></i>
+			</article>
 
 		</section>
-		<section class="full-width header-well" style="display: flex;">
+
+		<!-- Aquí se generan los articles dinámicamente Muestro cantidad por motivo en cada tarjeta-->
+		<?php while($fila = mysqli_fetch_array($resultado)){ ?>
+        <article class="full-width tile">
+            <div class="tile-text">
+                <span class="text-condensedLight">
+                    <?php echo $fila[1]; ?><br>
+                    <small> <?php echo $fila[0]; ?></small>
+                </span>
+            </div>
+            <i class="zmdi zmdi-account tile-icon"></i>
+        </article>
+    <?php } ?>
+
+		<!-- Coloco grafico de barras de motivos atendidos -->
+		<section class="full-width header-well" >
 			<div style="width: 30%; height: 30%;">
 				<canvas id="Motivos" width="100px" height="100px"></canvas>
 			</div>
-			<div style="width: 30%; height: 30%;">
-				<canvas id="empleados" width="100px" height="100px"></canvas>
-			</div>
 		</section>
 
-		
-		<section class="full-width header-well" style="display: flex;">
-			<div style="width: 30%; height: 30%;">
-				<canvas id="Motivos" width="100px" height="100px"></canvas>
-			</div>
+		<h3 class="text-center tittles">Clienes Por Empleado </h3>
+		<!-- Coloco grafico de torta -->
+		<section class="full-width header-well" >			
 			<div style="width: 30%; height: 30%;">
 				<canvas id="empleados" width="100px" height="100px"></canvas>
 			</div>
 		</section>
 		<section class="full-width text-center" style="padding: 40px 0;">
-    <h3 class="text-center tittles">Bienvenido</h3>
+
+    
     
     <!-- Aquí se generan los articles dinámicamente NO FUNCIONA -->
-    <?php foreach ($resultados as $motivo => $cantidad): ?>
+     <?php foreach ($resultados as $motivo => $cantidad): ?>
         <article class="full-width tile">
             <div class="tile-text">
                 <span class="text-condensedLight">
@@ -81,20 +97,9 @@ foreach ($motivos as $motivo) {
             </div>
             <i class="zmdi zmdi-account tile-icon"></i>
         </article>
-    <?php endforeach; ?>
+    <?php endforeach; ?> 
 
-	    <!-- Aquí se generan los articles dinámicamente -->
-		<?php while($fila = mysqli_fetch_array($resultado)){ ?>
-        <article class="full-width tile">
-            <div class="tile-text">
-                <span class="text-condensedLight">
-                    <?php echo $fila[1]; ?><br>
-                    <small>Clientes atendidos por <?php echo $fila[0]; ?></small>
-                </span>
-            </div>
-            <i class="zmdi zmdi-account tile-icon"></i>
-        </article>
-    <?php } ?>
+	    
     
 </section>
 <?php 
